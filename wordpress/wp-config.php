5f81fe2f77a863b94dfda6d7cb2084270d085fcb
root <root@buildstack.net>
2016-03-23 23:48:44 +0000

    made some buildstack specific changes to wordpress.  let's see if they persist..

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -42,25 +42,25 @@
 
 /** The name of the database for WordPress */
 
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'wordpress');
 
 
 
 /** MySQL database username */
 
-define('DB_USER', 'username_here');
+define('DB_USER', 'root');
 
 
 
 /** MySQL database password */
 
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'P@ssw0rd');
 
 
 
 /** MySQL hostname */
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql');
 
 
 
@@ -94,21 +94,21 @@ define('DB_COLLATE', '');
 
  */
 
-define('AUTH_KEY',         'put your unique phrase here');
+define('AUTH_KEY',         'b08952c73d4090bbe3e95bbed2995d75b4165dd7');
 
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('SECURE_AUTH_KEY',  '4bed81257f98bd8f5190272368001de67cdec6c7');
 
-define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('LOGGED_IN_KEY',    'bfc7d0fba185d2c3125b73f01ba02e3117ecd006');
 
-define('NONCE_KEY',        'put your unique phrase here');
+define('NONCE_KEY',        'c8d11a10bcc9f11ea72eaf15c13d43c0317817c0');
 
-define('AUTH_SALT',        'put your unique phrase here');
+define('AUTH_SALT',        'c28bf6bf33d98e4d203f0acce458d2249e70126d');
 
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'f000ec79192b06a6b7576cc6b1b7466f398e93bf');
 
-define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('LOGGED_IN_SALT',   '90846b81127c8911db6c5d7531d35fb762b8868f');
 
-define('NONCE_SALT',       'put your unique phrase here');
+define('NONCE_SALT',       'd0126a770f290dc667adf75988ade67a14aa4c9f');
 
 
 
@@ -159,6 +159,12 @@ $table_prefix  = 'wp_';
 define('WP_DEBUG', false);
 
 
+
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
 
 /* That's all, stop editing! Happy blogging. */
 
